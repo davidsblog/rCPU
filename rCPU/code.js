7593e88770b97dc475e20ac531f4f2038fdb64f0
david <david@debian.test>
2015-04-07 13:02:08 +0100

    First attempt at plotting a graph.

--- rCPU/code.js
+++ rCPU/code.js
@@ -1,5 +1,11 @@
+var cpuDataSet = new TimeSeries();
+
 $(function() {
   $("#status").text("");
+
+  get_cpu_use();
+  initChart(1);
+
   setInterval("get_cpu_use()", 500);
 });
 
@@ -12,5 +18,30 @@ function get_cpu_use()
 	}).done(function(data)
 	{
 		$("#counter").text(JSON.parse(data)[0]);
+	   cpuDataSet.append(new Date().getTime(), JSON.parse(data)[0]);
 	});
 }
+
+function initChart(cpuId)
+{
+  var seriesOptions =
+  { 
+     strokeStyle: 'rgba(255, 0, 0, 1)',
+     fillStyle: 'rgba(255, 0, 0, 0.1)',
+     lineWidth: 3 
+  };
+  var timeline = new SmoothieChart(
+  { 
+     millisPerPixel: 20,
+     grid:
+     {
+        strokeStyle: '#555555',
+        lineWidth: 1,
+        millisPerLine: 1000,
+        verticalSections: 4 
+     }
+  });
+  timeline.addTimeSeries(cpuDataSet, seriesOptions);
+  timeline.streamTo(document.getElementById('cpu'+cpuId), 500);
+}
+
